3dfe0270e2c7c0d925b002e6907216642927b4a2
Pawel Klasa <pawel.klasa@gmail.com>
2018-03-13 13:07:42 +0000

    src

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-console.log('hi');
+let val;   
+
+val = document.all[4];
+val = document.forms; // returns collection of all forms in the document
+val = document.links;
+//val = document.links[0].className;
+val = document.scripts[0].className;
+val = document.scripts[0].getAttribute('src');
+
+
+console.log(val);
