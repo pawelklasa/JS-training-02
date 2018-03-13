7b48a3011adfee48bcca20aa42976291d75528a0
Pawel Klasa <pawel.klasa@gmail.com>
2018-03-13 14:50:33 +0000

    init

--- app.js
+++ app.js
@@ -1,7 +1,35 @@
 // document.getElementsByClassName();
 
-const items = document.getElementsByClassName('item'); // returns HTMLcollection, just like an array
+// const items = document.getElementsByClassName('item'); // returns HTMLcollection, just like an array
 
-console.log(items);
+// console.log(items);
 
-console.log(items[1]); // selecting just one item from HTMLcollections
+// console.log(items[1]); // selecting just one item from HTMLcollections
+
+// items[1].style.color = 'red'; 
+// items[0].style.fontSize = '22px';
+
+
+//  document.getElementsByTagName
+
+//let lis = document.getElementsByTagName('div'); 
+
+// console.log(items[1]); 
+
+// items[1].style.color = 'red'; 
+// items[0].style.fontSize = '22px';
+
+// convert HTMLcollection into array
+
+// lis = Array.from(lis);
+
+// lis.reverse();
+
+// lis.forEach(function(li, index){
+//     console.log(li.className);
+//     li.textContent =  `${index}: Hello`;
+// });
+
+// console.log(lis);
+
+// querySelectorAll
