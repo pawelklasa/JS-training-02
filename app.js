d3fd186274166cba0d340166e770212ba154fc76
Pawel Klasa <pawel.klasa@gmail.com>
2018-03-13 14:04:38 +0000

    init

---
+++ app.js
@@ -0,0 +1,7 @@
+// document.getElementsByClassName();
+
+const items = document.getElementsByClassName('item'); // returns HTMLcollection, just like an array
+
+console.log(items);
+
+console.log(items[1]); // selecting just one item from HTMLcollections
