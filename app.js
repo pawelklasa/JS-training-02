9b406a99b8f8243a3601d83f37861ad2c9ece874
Pawel Klasa <pawel.klasa@gmail.com>
2018-03-16 09:48:22 +0000

    int

--- app.js
+++ app.js
@@ -4,10 +4,12 @@ const list = document.querySelector('.collection');
 addTask.addEventListener('click', fire);
 
 const li = document.createElement('li');
+li.textContent = 'I am a new list item';
+li.className = 'collection-item';
 
 function fire(e) {
   console.log('Clicked');
   e.preventDefault();
-  list.appendChild('<li></li>');
+  list.appendChild(li);
 }
 
